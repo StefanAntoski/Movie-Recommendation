c47261dbffea3eb4c8989d3b0a370cea3e6766ea
ViktorSpasovski <vixxa.mkd@gmail.com>
2016-06-13 16:09:04 +0200

    Splash Screen

---
+++ app/src/main/java/com/example/antoski/movierecommendation/Splash.java
@@ -0,0 +1,15 @@
+package com.example.antoski.movierecommendation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        super.onCreate(savedInstanceState);
+    }
+}
