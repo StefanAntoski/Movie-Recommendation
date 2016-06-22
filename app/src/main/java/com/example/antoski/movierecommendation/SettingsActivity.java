4388195bc12cf34b986020f28bcf9dc6b0464716
Stefan Antoski <stefan.antoski@hotmail.com>
2016-06-22 02:34:53 +0200

    99,9% Gotov proekt

--- app/src/main/java/com/example/antoski/movierecommendation/SettingsActivity.java
+++ app/src/main/java/com/example/antoski/movierecommendation/SettingsActivity.java
@@ -3,16 +3,23 @@ package com.example.antoski.movierecommendation;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -61,6 +68,37 @@ public class SettingsActivity extends AppCompatActivity {
                 }
                 else if(position == 1) {
 
+                    final Configuration config;
+                    final Resources res = SettingsActivity.this.getResources();
+                    config = new Configuration(res.getConfiguration());
+                    final DisplayMetrics dm = res.getDisplayMetrics();
+
+                    PopupMenu popupmenu = new PopupMenu(SettingsActivity.this,view);
+                    popupmenu.inflate(R.menu.languages);
+
+                    popupmenu.show();
+
+                    popupmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            if(item.getItemId() == R.id.AngliskiJazik)
+                            {
+                                config.locale = Locale.ENGLISH;
+                            }
+
+                            if(item.getItemId() == R.id.MakedonskiJazik)
+                            {
+                                android.content.res.Configuration conf = res.getConfiguration();
+
+                                conf.locale = new Locale("mk");
+                               // Toast.makeText(SettingsActivity.this,"Македонски",Toast.LENGTH_LONG).show();
+                                res.updateConfiguration(conf,dm);
+                                return false;
+
+                            }
+                       return false;
+                        }
+                    });
                 }
                 else if(position == 2) {
                     Intent intent = new Intent(SettingsActivity.this, ReportProblem.class);
